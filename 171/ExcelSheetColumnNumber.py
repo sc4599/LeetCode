220d7859beaf1004c93a98c7e01b2527f599cf07
songchao <sc4599@163.com>
2016-08-14 17:10:36 +0800

    init

---
+++ 171/ExcelSheetColumnNumber.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        import string
+        es = [x for x in string.uppercase]
+        r = 0
+        l = range(len(s))
+        l.reverse()
+        for i, w in zip(l, s):
+            r += 26 ** i * (es.index(w)+1)
+        return r
+
+
+if __name__ == "__main__":
+    cls = Solution()
+    s = 'AAAB'
+    print cls.titleToNumber(s)
+    l = ['0', '1', '2']
+    # print range(0, 6)
